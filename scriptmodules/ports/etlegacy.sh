b1f0489c222867c42b17dbaa2163362d41e0f2b8
G Derber <gd.github@drbr.org>
2020-06-15 11:33:05 -0400

    Add etlegacy 64 bit script; updates to etlegacy script

--- scriptmodules/ports/etlegacy.sh
+++ scriptmodules/ports/etlegacy.sh
@@ -14,11 +14,16 @@ rp_module_desc="etlegacy - ET: Legacy - A Fully compatable Wolfenstein: Enemy Te
 rp_module_licence="https://raw.githubusercontent.com/etlegacy/etlegacy/master/COPYING.txt"
 rp_module_help=""
 rp_module_section="exp"
-rp_module_flags="!all x86"
+rp_module_flags=""
 
+
+function _arch_etlegacy() {
+    # exact parsing from Makefile
+    echo "$(uname -m | sed -e 's/i.86/x86/' | sed -e 's/^arm.*/arm/')"
+}
 
 function depends_etlegacy() {
-    getDepends cmake libsdl2-dev libopenal-dev #libc6-dev-i386 libx11-dev:i386 libgl1-mesa-dev:i386
+    getDepends cmake libsdl2-dev libopenal-dev libc6-dev-i386 libx11-dev:i386 libgl1-mesa-dev:i386
 }
 
 function sources_etlegacy() {
@@ -26,32 +31,44 @@ function sources_etlegacy() {
 }
 
 function build_etlegacy() {
+    local params=(-DCMAKE_BUILD_TYPE=Release -DINSTALL_DEFAULT_BINDIR="$md_inst/" -DINSTALL_DEFAULT_BASEDIR="$romdir/ports/etlegacy")
+
+    if [[ "${md_id}" == "etlegacy_64"]]; then
+        params+=(-DCROSS_COMPILE32=0)
+    else
+        params+=(-DCROSS_COMPILE32=1)
+    fi
+
+    if isPlatform "rpi"; then
+        params+=(-DARM=1)
+    fi
+
     mkdir "$md_build/build"
     cd "$md_build/build"
-    cmake -D INSTALL_DEFAULT_BINDIR="/opt/retropie/ports/etlegacy/bin/" -D INSTALL_DEFAULT_MODDIR="/opt/retropie/ports/etlegacy/lib/" ..
-    cd "$md_build"
+
+    echo "${params[@]}"
+
+    cmake "${params[@]}" ..
+    make clean
     make
-    md_ret_require="$md_build/build"
+
+    md_ret_require="$md_build/build/etl"
 }
 
 function install_etlegacy() {
     md_ret_files=(
-        "build/RBDoom3BFG"
-        "base/default.cfg"
-        "base/extract_resources.cfg"
-        "base/renderprogs"
+        "build/etl"
+        "build/etlded"
+        "build/librenderer_opengl1_$(_arch_etlegacy)"
     )
 }
 
 function game_data_etlegacy() {
-	  mkdir /opt/retropie/ports/etlegacy/base/
-
-    # https://cdn.splashdamage.com/downloads/games/wet/et260b.x86_full.zip
-
+    downloadAndExtract "https://cdn.splashdamage.com/downloads/games/wet/et260b.x86_full.zip" "$romdi/ports/etlegacy"
 }
 
 function configure_etlegacy() {
-    addPort "doom_3_bfg" "doom_3_bfg" "Doom 3 - BFG" "$md_inst/"
+    addPort "$md_id" "etlegacy" "Wolfenstein - Enemy Territory" "$md_inst/"
 
     mkRomDir "ports/etlegacy"
 
