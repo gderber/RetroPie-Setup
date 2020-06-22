30071bd73f33632d857693d6103469156e76c209
G Derber <gd.github@drbr.org>
2020-06-22 10:42:59 -0400

    Fix build files

--- scriptmodules/ports/openmw.sh
+++ scriptmodules/ports/openmw.sh
@@ -15,8 +15,18 @@ rp_module_licence="GPL3 https://gitlab.com/OpenMW/openmw/-/raw/master/LICENSE"
 rp_module_section="exp"
 rp_module_flags=""
 
+function _arch_openmw() {
+    # exact parsing from Makefile
+    echo "$(uname -m | sed -e 's/i.86/x86/' | sed -e 's/^arm.*/arm/')"
+}
+
 function depends_openmw() {
-    getDepends libsdl2-dev libgl1-mesa-dev
+    local depends=(libopenal-dev libopenscenegraph-3.4-dev libsdl2-dev libqt4-dev)
+    depends+=(libboost-filesystem-dev libboost-thread-dev libboost-program-options-dev)
+    depends+=(libboost-system-dev libboost-iostreams-dev libavcodec-dev libavformat-dev)
+    depends+=(libavutil-dev libswscale-dev libswresample-dev libbullet-dev libmygui-dev)
+    depends+=(libunshield-dev cmake build-essential libqt4-opengl-dev libtinyxml-dev)
+    getDepends $depends
 }
 
 function sources_openmw() {
@@ -30,23 +40,29 @@ function build_openmw() {
 
     make clean
     make
-}
 
-function _arch_openmw() {
-    # exact parsing from Makefile
-    echo "$(uname -m | sed -e 's/i.86/x86/' | sed -e 's/^arm.*/arm/')"
+    md_ret_require="$md_build/build/openwm"
 }
 
 function install_openmw() {
     md_ret_files=(
-        "build/release-linux-$(_arch_openmw)/openmw.$(_arch_openmw)"
-    )
+        "build/openmw"
+        "build/openmw-cs"
+        "build/bsatool"
+        "build/esmtool"
+        "build/niftest"
+        "build/openmw-essimporter"
+        "build/openmw-iniimporter"
+        "build/openmw-launcher"
+        "build/openmw-wizard"
+        )
 }
 
 function configure_openmw() {
-    local launcher=("$md_inst/openmw.$(_arch_openmw)")
+    local launcher=("$md_inst/openmw-launcher")
 
     addPort "$md_id" "morrowind" "The Elder Scrolls III - Morrowind" "${launcher[*]}"
 
     mkRomDir "ports/morrowind"
+    moveConfigDir "$md_inst/data" "$romdir/ports/jediacademy"
 }
